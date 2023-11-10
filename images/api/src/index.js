07fa084169a92cfc76c778441ff031b3f0db791a
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2023-11-10 11:24:12 +0100

    feat(api): implement POST endpoint for creating a new student

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -19,10 +19,47 @@ app.use(express.json());
  * @returns
  */
 
-
+//this works!
 app.get("/", (request, response) => {
    response.send({message: "hello world"})
 })
+
+/**
+ * POST endpoint for creating a new student.
+ * 
+ * @param - The HTTP request object.
+ * @param - The HTTP response object.
+ * @returns - The HTTP response containing either a success message or an error.
+ */
+
+app.post('/api/students', async (req, res) => {
+
+  if (!req.body) {
+      return res.status(400).send({
+          error: "Request body is missing or empty",
+      });
+  }
+
+  const { id, first_name, last_name, age, email } = req.body;
+  try {
+      await db('students').insert({
+          id,
+          first_name,
+          last_name,
+          age,
+          email,
+      });
+      res.status(201).send({
+          message: 'Student created successfully',
+      });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send({
+          error: "Something went wrong",
+          value: error,
+      });
+  }
+});
 
 
 app.listen(3000, (error)=> {
