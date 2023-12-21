3e5567b8773750e011736385837b99e19c630f4c
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2023-12-21 19:38:38 +0100

    feat(putRoute): implement PUT endpoint for updating fitness information by ID

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -24,95 +24,6 @@ app.get("/", (request, response) => {
    response.send({message: "hello world"})
 })
 
-/**
- * GET endpoint for retrieving all students.
- * 
- * @param - The HTTP request object.
- * @param - The HTTP response object.
- * @returns - The HTTP response containing a list of students or an error message.
- */
-
-app.get('/api/students', async (req, res) => {
-  try {
-    const students = await db('students')
-
-    res.status(200).send(students)
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).send({
-      error: "Something went wrong",
-      value: error
-    });
-  }
-});
-
-/**
- * GET endpoint for retrieving a specific student by ID.
- * 
- * @param - The HTTP request object.
- * @param - The HTTP response object.
- * @returns - The HTTP response containing the requested student's information or an error message.
- */
-
-app.get('/api/students/:id', async (req, res) => {
-const studentId = req.params.id;
-try {
-  const student = await db('students').where('id', studentId).first();
-
-  if(!student) {
-    return res.status(404).send({
-      error: "Student not found",
-    });
-  }
-
-  res.status(200).send(student)
-} catch (error) {
-  console.log(error);
-
-  res.status(500).send({
-    error: "Something went wrong",
-    value: error
-  });
-}
-});
-
-/**
- * POST endpoint for creating a new student.
- * 
- * @param - The HTTP request object.
- * @param - The HTTP response object.
- * @returns - The HTTP response containing either a success message or an error.
- */
-
-app.post('/api/students', async (req, res) => {
-
-  if (!req.body) {
-      return res.status(400).send({
-          error: "Request body is missing or empty",
-      });
-  }
-
-  const { id, first_name, last_name, age, email } = req.body;
-  try {
-      await db('students').insert({
-          id,
-          first_name,
-          last_name,
-          age,
-          email,
-      });
-      res.status(201).send({
-          message: 'Student created successfully',
-      });
-  } catch (error) {
-      console.error(error);
-      res.status(500).send({
-          error: "Something went wrong",
-          value: error,
-      });
-  }
-});
 
 
 /**
@@ -155,6 +66,64 @@ app.put('/api/students/:id', async (req, res) => {
   }
 });
 
+/**
+ * PUT endpoint for updating fitness information by ID.
+ * 
+ * @param - The HTTP request object.
+ * @param - The HTTP response object.
+ * @returns - The HTTP response containing either a success message or an error.
+ */
+app.put('/api/fitness_info/:id', async (req, res) => {
+    const fitnessId = req.params.id;
+  
+    // Ensure that the request body is not empty
+    if (!req.body) {
+      return res.status(400).send({
+        error: "Request body is missing or empty",
+      });
+    }
+  
+    // Destructure the parameters from the request body
+    const { student_id, physical_activity, exercise_duration, anxiety_control, sleep_duration, quality_of_sleep } = req.body;
+  
+    try {
+      // Check if the fitness information exists
+      const existingFitnessInfo = await db('fitness_info').where({ id: fitnessId }).first();
+  
+      if (!existingFitnessInfo) {
+        return res.status(404).send({
+          error: "Fitness information not found",
+        });
+      }
+  
+      // Update the fitness information in the 'fitness_info' table
+      await db('fitness_info').where({ id: fitnessId }).update({
+        student_id,
+        physical_activity,
+        exercise_duration,
+        anxiety_control,
+        sleep_duration,
+        quality_of_sleep,
+      });
+  
+      // Log a message to the terminal
+      console.log('Fitness info updated successfully:', req.body);
+  
+      // Send a success response
+      res.status(200).send({
+        message: 'Fitness info updated successfully',
+      });
+    } catch (error) {
+      // Handle errors and send an error response
+      console.error(error);
+      res.status(500).send({
+        error: "Something went wrong",
+        value: error,
+      });
+    }
+  });
+
+  
 app.listen(3000, (error)=> {
     if(!error){
         console.log("running on port " + 3000);
